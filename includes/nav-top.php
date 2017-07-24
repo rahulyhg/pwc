eb73601d70ca1a20c562d680b6a6b84e972c92c1
GST Local <gstlocal@GauravSTomars-MacBook.local>
2017-07-24 09:32:13 +0530

    first commit

---
+++ includes/nav-top.php
@@ -0,0 +1,58 @@
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">Read</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services.php">Watch</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Meet</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Kriya
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                            <a class="dropdown-item" href="portfolio-1-col.php">1 Column Portfolio</a>
+                            <a class="dropdown-item" href="portfolio-2-col.php">2 Column Portfolio</a>
+                            <a class="dropdown-item" href="portfolio-3-col.php">3 Column Portfolio</a>
+                            <a class="dropdown-item" href="portfolio-4-col.php">4 Column Portfolio</a>
+                            <a class="dropdown-item" href="portfolio-item.php">Single Portfolio Item</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Outreach
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                            <a class="dropdown-item" href="blog-home-1.php">Blog Home 1</a>
+                            <a class="dropdown-item" href="blog-home-2.php">Blog Home 2</a>
+                            <a class="dropdown-item" href="blog-post.php">Blog Post</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Center
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                            <a class="dropdown-item" href="full-width.php">Full Width Page</a>
+                            <a class="dropdown-item" href="sidebar.php">Sidebar Page</a>
+                            <a class="dropdown-item" href="faq.php">FAQ</a>
+                            <a class="dropdown-item" href="404.php">404</a>
+                            <a class="dropdown-item" href="pricing.php">Pricing Table</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
