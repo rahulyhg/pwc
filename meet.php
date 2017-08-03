c3f99a7e69edba4cdd7c8f9dd73a41078d60e78c
GST Local <gstlocal@GauravSTomars-MacBook.local>
2017-08-03 11:42:51 +0530

    meet and video pages updated

--- meet.php
+++ meet.php
@@ -4,14 +4,15 @@ require("includes/head.php")
 
 
 <body>
+
     <!-- Navigation -->
     <?php
     require("includes/nav-top.php")
     ?>
 
-
     <!-- Page Content -->
     <div class="container">
+
 
         <!-- Page Heading/Breadcrumbs -->
         <h1 class="mt-4 mb-3">Meet <small>Upcoming Camps</small></h1>
@@ -25,49 +26,54 @@ require("includes/head.php")
 
         <div class="row">
 
+        <div class="col-md-8">
+
 <?
-    for ($i=0; $i<3; $i++) { 
+    for ($i=0; $i<5; $i++) { 
+
 ?>
-            <div class="col-lg-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
-                    <div class="card-block">
-                        <h4 class="card-title"><a href="#">City name - Shivir One</a></h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            <!-- Blog Post -->
+            <div class="card mb-4">
+                <div class="card-block">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="#">
+                                <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-6">
+                            <h2 class="card-title"><a href="#">City name - Shivir One</a></h2>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                        </div>
                     </div>
                 </div>
+                <div class="card-footer text-muted">
+                    Posted on January 1, 2017
+                </div>
             </div>
-            
 <?
     }
-?>            
+?>
 
+            <!-- Pagination -->
+            <ul class="pagination justify-content-center mb-4">
+                <li class="page-item"><a class="page-link" href="#">&larr; Older</a></li>
+                <li class="page-item disabled"><a class="page-link" href="#">Newer &rarr;</a></li>
+            </ul>
+            
         </div>
-        <!-- /.row -->
 
-        <!-- Pagination -->
-        <ul class="pagination justify-content-center">
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                    <span class="sr-only">Previous</span>
-                </a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </li>
-        </ul>
+            <!-- Sidebar Widgets Column -->
+            <?php
+            require("includes/blog-sidebar.php")
+            ?>
+    </div>
+    </div>
 
     </div>
     <!-- /.container -->
 
-   <!-- Footer -->
+    <!-- Footer -->
     <?php
     require("includes/footer.php")
     ?>
--- watch.php
+++ watch.php
@@ -4,15 +4,16 @@ require("includes/head.php")
 
 
 <body>
-
     <!-- Navigation -->
     <?php
     require("includes/nav-top.php")
     ?>
 
+
     <!-- Page Content -->
     <div class="container">
 
+        
         <!-- Page Heading/Breadcrumbs -->
         <h1 class="mt-4 mb-3">Watch <small>Updates</small></h1>
 
@@ -24,55 +25,55 @@ require("includes/head.php")
         </ol>
 
         <div class="row">
-
-        <div class="col-md-8">
-
-<?
-    for ($i=0; $i<5; $i++) { 
-
-?>
-            <!-- Blog Post -->
-            <div class="card mb-4">
-                <div class="card-block">
+            <div class="col-md-8">
                     <div class="row">
-                        <div class="col-lg-6">
-                            <a href="#">
-                                <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
-                            </a>
-                        </div>
-                        <div class="col-lg-6">
-                            <h2 class="card-title">Video Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                        </div>
+<?
+    for ($i=0; $i<3; $i++) { 
+?>
+            <div class="col-lg-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-block">
+                        <h4 class="card-title">Video Title</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
                     </div>
-                </div>
-                <div class="card-footer text-muted">
-                    Posted on January 1, 2017
                 </div>
             </div>
 <?
     }
 ?>
+            </div>
 
-            <!-- Pagination -->
-            <ul class="pagination justify-content-center mb-4">
-                <li class="page-item"><a class="page-link" href="#">&larr; Older</a></li>
-                <li class="page-item disabled"><a class="page-link" href="#">Newer &rarr;</a></li>
-            </ul>
-            
-        </div>
+        <!-- Pagination -->
+        <ul class="pagination justify-content-center">
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </li>
+        </ul>
 
+            </div>
             <!-- Sidebar Widgets Column -->
-            <?php
-            require("includes/blog-sidebar.php")
-            ?>
-    </div>
-    </div>
-
+        <?php
+        require("includes/blog-sidebar.php")
+        ?>
+        </div>
+        <!-- /.row -->
     </div>
     <!-- /.container -->
 
-    <!-- Footer -->
+   <!-- Footer -->
     <?php
     require("includes/footer.php")
     ?>
